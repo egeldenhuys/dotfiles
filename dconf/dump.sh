11cddcf0f0c6311ae8c3bf7b014c79beeb2ae44c
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2019-08-10 12:21:27 +0200

    Gnome terminal

--- dconf/dump.sh
+++ dconf/dump.sh
@@ -2,3 +2,4 @@
 
 dconf dump /org/cinnamon/ > cinnamon.dconf
 dconf dump /org/nemo/ > nemo.dconf
+dconf dump /org/gnome/terminal/ > gnome-terminal.dconf
--- dconf/load.sh
+++ dconf/load.sh
@@ -2,3 +2,4 @@
 
 dconf load /org/cinnamon/ < cinnamon.dconf
 dconf load /org/nemo/ < nemo.dconf
+dconf load /org/gnome/terminal/ < gnome-terminal.dconf
